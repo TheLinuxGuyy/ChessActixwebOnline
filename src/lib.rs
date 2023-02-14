8f117839c3a808b75a8751397b84c2e3d1ff2b9b
Clive <clive@pop-os.localdomain>
2023-02-14 23:41:23 +0000

    commit

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,12 @@ use wasm_bindgen::prelude::*;
 pub struct Chess;
 
 impl Chess{
-    pub fn move(peice: String, from: String, to:String)
+    pub fn moveTo(peice: String, from: String, to:String){
+        
+    }
 }
 
 #[wasm_bindgen]
-pub fn add(a: i32,b:i32) -> i32{
-    a + b
+pub fn move() -> i32{
+    
 }
