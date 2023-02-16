7bc25aec2bc39e75ba3b81451376fc008621ecda
Clive <clive@pop-os.localdomain>
2023-02-16 19:40:05 +0000

    commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
 use wasm_bindgen::prelude::*;
 
-pub struct Chess;
 
-impl Chess{
-    pub fn moveTo(peice: String, from: String, to:String){
-        
-    }
-}
-
-#[wasm_bindgen]
-pub fn move() -> i32{
-    
-}
