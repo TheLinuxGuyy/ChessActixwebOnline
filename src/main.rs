77f2f62315be753133e8a922e1a64b9bc404fc3c
Clive <clive@pop-os.localdomain>
2023-02-18 16:06:43 +0000

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,21 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web,HttpResponse,http::StatusCode ,App, HttpServer, Responder};
+use tera::{Tera,Context};
+use actix_web::middleware::Logger;
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn greet(name: web::Path<String>) -> impl Responder {
+    
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG","debug");
+    std::env::set_var("RUST_BACKTRACE","1");
+    env_logger::init();
     HttpServer::new(|| {
+        let logger = Logger::default();
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .wrap(logger);
+        App::new().service(greet)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
