2d4332683280d6fe3cac970ff2cb2653b84172d2
Clive <117390912+TheLinuxGuyy@users.noreply.github.com>
2023-03-03 23:44:23 +0000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -29,13 +29,28 @@ fn checking_legality(p:String,f:String,t:String){
     let result=match p{
         "pawn" => {
             let mut factory_positions = vec!["a7","b7","c7","d7","e7","f7","g7","h7","a2","b2","c2","d2","e2","f2","g2","h2"];
+            let number_from: i32 = f[1].parse().unwrap();
             let number_to: i32 = t[1].parse().unwrap();
+            let letter_from: String = f[0].parse().unwrap();
+            let letter_to: String = t[0].parse.unwrap();
+            let letter_to_index = alphabet.iter().position(|&r| r == letter_to).unwrap();
+            let letter_from_index = alphabet.iter().position(|&r| r == letter_from).unwrap();
             if(f in factory_positions){
-                if(t==f.replace(number_to.to_string(),number_to+1.to_string()) || f.replace(number_to.to_string(),number_to+2.to_string()){
-                "legal"
+                if(t==f[1].replace(number_to.to_string(),number_to+1.to_string()) || f.replace(number_to.to_string(),number_to+2.to_string()){
+                    "legal" //going forward twice or once
                 }else{
+                    "illegal" // if you dont go forward once or twice at the start of the round, it is illegal
+                }
+            }else{
+                if(number_from>number_to){ //going backwards
                     "illegal"
                 }
+
+                if(letter_to != letter_from){
+                    // if it changes into a differant row, it is illegal, but if an enemy is in the other row, it is legal
+                }
+
+
             }
 
         }
