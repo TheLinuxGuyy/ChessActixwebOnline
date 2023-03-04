92c01c1b46b94717541d8d76f0f097281588b128
Clive <clive@pop-os.localdomain>
2023-03-04 18:19:02 +0000

    commit

--- src/lib.rs
+++ src/lib.rs
@@ -24,19 +24,19 @@ pub fn connect(lobby_number) {
     listening(&socket);
 }
 
-fn checking_legality(p:String,f:String,t:String,toccupied:bool){
+fn checking_legality(p:String,f:String,t:String,toccupied:bool){ //toccupied should be true if it is an opposit color to the color that is going into the square 
     alphabet = ["a","b","c","d","e","f","g","h"]
+    let number_from: i32 = f[1].parse().unwrap();
+    let number_to: i32 = t[1].parse().unwrap();
+    let letter_from: String = f[0].parse().unwrap();
+    let letter_to: String = t[0].parse.unwrap();
+    let letter_to_index = alphabet.iter().position(|&r| r == letter_to).unwrap();
+    let letter_from_index = alphabet.iter().position(|&r| r == letter_from).unwrap();
     let result=match p{
         "pawn" => {
             let factory_positions = vec!["a7","b7","c7","d7","e7","f7","g7","h7","a2","b2","c2","d2","e2","f2","g2","h2"];
-            let number_from: i32 = f[1].parse().unwrap();
-            let number_to: i32 = t[1].parse().unwrap();
-            let letter_from: String = f[0].parse().unwrap();
-            let letter_to: String = t[0].parse.unwrap();
-            let letter_to_index = alphabet.iter().position(|&r| r == letter_to).unwrap();
-            let letter_from_index = alphabet.iter().position(|&r| r == letter_from).unwrap();
             if(f in factory_positions){
-                if(t==f[1].replace(number_to.to_string(),number_to+1.to_string()) || f.replace(number_to.to_string(),number_to+2.to_string()){
+                if(t==f.replace(number_to.to_string(),number_to+1.to_string()) || f.replace(number_to.to_string(),number_to+2.to_string()){
                     "legal" //going forward twice or once
                 }else{
                     "illegal" // if you dont go forward once or twice at the start of the round, it is illegal
@@ -54,10 +54,11 @@ fn checking_legality(p:String,f:String,t:String,toccupied:bool){
                         "illegal"
                     }
                 }
+                if(t==f[1].replace(number_to.to_string(),number_to+1.to_string())
             }
         }
         "knight" =>{
-            let 
+            let factory_positions = vec!["g1","b1","b8","g8"]
         }
         "rook" => checking_legality("rook"),
         "bishop" => checking_legality("bishop"),
@@ -65,7 +66,6 @@ fn checking_legality(p:String,f:String,t:String,toccupied:bool){
         "queen" => checking_legality("queen"),
         _ => "not a vaild peice",
     }
-    result
 }
 
 #[wasm_bindgen]
